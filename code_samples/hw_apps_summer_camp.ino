0018cc7d36e153b953b249cd2ace81d01867b6b4
karyna-volokhatiuk <71793550+karyna-volokhatiuk@users.noreply.github.com>
2021-08-20 11:27:09 +0300

    Update hw_apps_summer_camp.ino

--- code_samples/hw_apps_summer_camp.ino
+++ code_samples/hw_apps_summer_camp.ino
@@ -5,29 +5,39 @@
  pin 11 is connected to the CLK 
  pin 10 is connected to LOAD 
  */
-LedControl lc=LedControl(12,11,10,1);
+
+//pins
+#define DataIn 12
+#define CLK 11
+#define CS 10
+#define COUNT_MATRICES 1
+
+#define DELAY_TIME 100
+
+#define SIZE_MATRIX 8
+#define MATRIX_0 0
+
+LedControl lc=LedControl(DataIn,CLK,CS,COUNT_MATRICES);
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-#define delaytime 1000
-#define size_matrix 8
 
-byte square_2[size_matrix] = {byte(0), byte(0), byte(0), B00011000, B00011000, byte(0), byte(0), byte(0)};
-byte square_4[size_matrix] = {byte(0), byte(0), B00111100, B00100100, B00100100, B00111100, byte(0), byte(0)};
-byte square_6[size_matrix] = {byte(0), B01111110, B01000010, B01000010, B01000010, B01000010, B01111110, byte(0)};
-byte square_8[size_matrix] = {B11111111, B10000001, B10000001, B10000001, B10000001, B10000001, B10000001, B11111111};
+byte square_2[SIZE_MATRIX] = {byte(0), byte(0), byte(0), B00011000, B00011000, byte(0), byte(0), byte(0)};
+byte square_4[SIZE_MATRIX] = {byte(0), byte(0), B00111100, B00100100, B00100100, B00111100, byte(0), byte(0)};
+byte square_6[SIZE_MATRIX] = {byte(0), B01111110, B01000010, B01000010, B01000010, B01000010, B01111110, byte(0)};
+byte square_8[SIZE_MATRIX] = {B11111111, B10000001, B10000001, B10000001, B10000001, B10000001, B10000001, B11111111};
                                              
-void writeSquare(byte square_array[size_matrix]);
+void writeSquare(byte square_array[SIZE_MATRIX]);
 
 void setup() {
   /*
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
-  lc.shutdown(0,false);
+  lc.shutdown(MATRIX_0,false);
   /* Set the brightness in range (0, 15)*/
-  lc.setIntensity(0,0);
+  lc.setIntensity(MATRIX_0,0);
   /* and clear the display */
-  lc.clearDisplay(0);
+  lc.clearDisplay(MATRIX_0);
 
   //settings for VL53L0X and Serial
   Serial.begin(115200);
@@ -59,23 +69,22 @@ void loop() {
   }
 }
 
-void writeSquare(int num_matrix, byte square_array[size_matrix]){
-  for (int i=0; i<size_matrix; i++){
+void writeSquare(int num_matrix, byte square_array[SIZE_MATRIX]){
+  for (int i=0; i<SIZE_MATRIX; i++){
     lc.setRow(num_matrix, i, square_array[i]);
   }
-  delay(delaytime);
-  lc.clearDisplay(0);
-  delay(delaytime);
+  delay(DELAY_TIME);
 }
 
 void fromDistanceToSquare(int distance){
-  if (distance < 20){
-    writeSquare(0, square_2);
-  } else if (distance < 50){
-    writeSquare(0, square_4);
-  } else if (distance < 100){
-    writeSquare(0, square_6);
+  lc.clearDisplay(0);
+  if (distance < 40){
+    writeSquare(MATRIX_0, square_2);
+  } else if (distance < 80){
+    writeSquare(MATRIX_0, square_4);
+  } else if (distance < 120){
+    writeSquare(MATRIX_0, square_6);
   }else{
-    writeSquare(0, square_8);
+    writeSquare(MATRIX_0, square_8);
   }
 }
